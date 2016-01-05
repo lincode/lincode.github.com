53c5e473032774bdc91b0e79d56cd383cd91853f
guolin <guolin@douban.com>
2016-01-05 17:48:59 +0800

    add disque

--- js/post.js
+++ js/post.js
@@ -23,6 +23,13 @@ $(document).ready(function(){
 
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
+    window.disqus_shortname = 'lincode'; // required: replace example with your forum shortname
+    $('#disqus_container .comment').on('click',function(){
+        $(this).html('加载中...');
+        var that = this;
+        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+    });
+
     $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
         if(href){
